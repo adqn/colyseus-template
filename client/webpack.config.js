3ef8c56514a0eaf848deece5b2e2bd19e75cb51b
adqn <57331120+adqn@users.noreply.github.com>
2022-07-29 11:08:32 -0700

    Added webpack-dev-server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: "./src/index.tsx",
+  devServer: {
+    static: "../dist",
+  },
   module: {
     rules: [
       {
@@ -28,7 +31,10 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
